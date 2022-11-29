fc43b023bb2f235c6f05816edac8cd8e6f690b89
wilsonwesley <wesley-wilson@outlook.com>
2022-11-29 11:11:48 +0100

    Debug view

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import Home from './src/screens/Home.js';
+import Register from './src/screens/Register.js';
+
 
 function App() {
   return (
     // la page de connexion devrait être la 1ere page à être affichée
     // lors de l'arrivée sur l'appli
     <Home />
+    // <Register />
   );
 }
 
