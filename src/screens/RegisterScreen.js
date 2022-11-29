09f89a51196301682a6aaae4dd1856505c7e635b
wilsonwesley <wesley-wilson@outlook.com>
2022-11-29 16:34:38 +0100

    Start adding form checking

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { Button, View } from 'react-native';
 import { Formik } from 'formik';
-import { Input, Stack } from "native-base";
+import * as yup from 'yup';
+
+import { Input, Stack, FormControl, WarningOutlineIcon } from "native-base";
 import { register } from "../services/users";
+
+// const validationSchema plutot ??
+const registerSchema = yup.object({
+    firstName: yup.string().required('Ce champ est obligatoire'),
+    lastName: yup.string().required('Ce champ est obligatoire'),
+    email: yup
+        .string('Ce champ est obligatoire')
+        .email('La saisie est invalide.')
+        .required('Ce champ est obligatoire'),
+    password: yup.string().required('Ce champ est obligatoire'),
+    passwordConfirm: yup
+        .string()
+        .required('Ce champ est obligatoire')
+        .oneOf(
+            [yup.ref('password'), null],
+            'Les mots de passes ne correspondent pas.'
+        ),
+});
 
 export const RegisterScreen = props => (
     <Formik
+        // pas sur de a ligne ci dessous
+        validationSchema={registerSchema}
         initialValues={{ 
             firstName: '',
             lastName: '',
@@ -17,50 +39,88 @@ export const RegisterScreen = props => (
         onSubmit={ values => {
             delete values.passwordConfirm;
             register(values).then(()=>{
-                console.log('Création succes')
+                console.log('L\'utilisateur à bien été enregistré.')
             }).catch(err => {
                 console.log(err)
             })
         }}
     >
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
+      {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+
         <View style={{ flex: 1, justifyContent: "center" }}>
             <Stack space={2} w="75%" maxW="300px" mx="auto">
-                
-                <Input  variant="underlined" 
-                        type="text" 
-                        placeholder="Prénom"
-                        onChangeText={handleChange('firstName')}
-                        onBlur={handleBlur('firstName')}
-                        value={values.firstname} />
 
-                <Input  variant="underlined" 
-                        type="text" 
-                        placeholder="Nom"
-                        onChangeText={handleChange('lastName')}
-                        onBlur={handleBlur('lastName')}
-                        value={values.lastname} />
 
-                <Input  variant="underlined" 
-                        type="email" 
-                        placeholder="Email" 
-                        onChangeText={handleChange('email')}
-                        onBlur={handleBlur('email')}
-                        value={values.email} />
+            <FormControl isInvalid={errors}>
+                <FormControl.Label >Prénom</FormControl.Label>
+                    <Input  variant="underlined" 
+                            type="text" 
+                            placeholder="Jean"
+                            onChangeText={handleChange('firstName')}
+                            onBlur={handleBlur('firstName')}
+                            value={values.firstname} />
+                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                    Something is wrong.
+                </FormControl.ErrorMessage>
+            </FormControl>
 
-                <Input  variant="underlined" 
-                        type="password" 
-                        placeholder="Mot de passe"
-                        onChangeText={handleChange('password')}
-                        onBlur={handleBlur('password')}
-                        value={values.password} />
 
-                <Input  variant="underlined" 
-                        type="password" 
-                        placeholder="Confirmation du mot de passe"
-                        onChangeText={handleChange('passwordConfirm')}
-                        onBlur={handleBlur('passwordConfirm')}
-                        value={values.passwordConfirm} />
+            <FormControl isInvalid={errors}>
+                <FormControl.Label>Nom</FormControl.Label>
+                    <Input  variant="underlined" 
+                            type="text" 
+                            placeholder="BONHERTA"
+                            onChangeText={handleChange('lastName')}
+                            onBlur={handleBlur('lastName')}
+                            value={values.lastname} />
+                    
+                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                    Something is wrong.
+                </FormControl.ErrorMessage>
+            </FormControl>
+
+
+
+            <FormControl isInvalid={errors}>
+                <FormControl.Label>Adresse email</FormControl.Label>
+                    <Input  variant="underlined" 
+                            type="email" 
+                            placeholder="jbon@herta.fr" 
+                            onChangeText={handleChange('email')}
+                            onBlur={handleBlur('email')}
+                            value={values.email} />
+                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                    Something is wrong.
+                </FormControl.ErrorMessage>
+            </FormControl>
+
+
+            <FormControl isInvalid={errors}>
+                <FormControl.Label>Mot de passe</FormControl.Label>
+                    <Input  variant="underlined" 
+                            type="password" 
+                            placeholder="********"
+                            onChangeText={handleChange('password')}
+                            onBlur={handleBlur('password')}
+                            value={values.password} />   
+                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                    Something is wrong.
+                </FormControl.ErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={errors}>
+                <FormControl.Label>Confirmation de mot de passe</FormControl.Label>
+                    <Input  variant="underlined" 
+                            type="password" 
+                            placeholder="********"
+                            onChangeText={handleChange('passwordConfirm')}
+                            onBlur={handleBlur('passwordConfirm')}
+                            value={values.passwordConfirm} />  
+                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                    Something is wrong.
+                </FormControl.ErrorMessage>
+            </FormControl>
+
 
                 <Button onPress={handleSubmit} title="Enregistrer" />
             </Stack>
