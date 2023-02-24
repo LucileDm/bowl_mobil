b051572d021207f28a57cab6bf1ea1e88e852b5e
wilsonwesley <wesley-wilson@outlook.com>
2023-02-24 15:20:22 +0100

    Import search bar usefull element

--- src/screens/Restaurant/RestaurantListScreen.js
+++ src/screens/Restaurant/RestaurantListScreen.js
@@ -1,9 +1,19 @@
 import { React, useEffect, useState } from "react";
-import { FlatList, Heading, HStack, VStack } from "native-base";
+import {
+  Box,
+  Divider,
+  FlatList,
+  Heading,
+  HStack,
+  Icon,
+  Input,
+  VStack,
+} from "native-base";
 
 import { getAllRestaurants } from "./../../services/restaurants";
 
 import ItemListRestaurant from "../../components/ItemListRestaurant";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 const RestaurantListScreen = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -33,7 +43,6 @@ const RestaurantListScreen = () => {
           <Heading>villes</Heading>
         </VStack>
       </HStack>
-
       <FlatList
         data={restaurants}
         key={(item, index) => index}
