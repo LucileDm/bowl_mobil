74f100ee9f62f87b262dbba601cd37af97b01db3
Lucile <sirius_86@outlook.fr>
2023-03-23 18:30:32 +0100

    Navigation handled by errorHandler

--- App.js
+++ App.js
@@ -26,4 +26,8 @@ function App() {
   );
 }
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
+
 export default App;
