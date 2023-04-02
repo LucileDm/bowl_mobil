fc231e0f38a9f8ddf250f8f1fcf6f23946862ce7
Lucile <sirius_86@outlook.fr>
2023-04-02 16:47:18 +0200

    Adding routeProtector code

--- src/navigations/Stacks/BookingStackNavigation.js
+++ src/navigations/Stacks/BookingStackNavigation.js
@@ -1,9 +1,10 @@
 const Stack = createNativeStackNavigator();
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BookingScreen from '../../screens/Booking/BookingScreen.js';
-// import AddBooking from "../../screens/Booking/AddBooking";
-// import ListBookings from "../../screens/Booking/ListBookings";
+import AddBooking from "../../screens/Booking/AddBooking";
+import ListBookings from "../../screens/Booking/ListBookings";
 import TextLogo from "../../components/TextLogo.js";
+import RouteProtector from './components/RouteProtector';
 
 const headerTitle = { headerTitle: () => <TextLogo /> };
 
@@ -11,22 +12,45 @@ function BookingStackNavigation() {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name='Reservation'
-        component={BookingScreen}
+        name='Reservations'
+        component={
+          <RouteProtector permittedRoles={['ROLE_USER']}>
+            {BookingScreen}
+          </RouteProtector>
+        }
         options={headerTitle}
         />
       
-      {/* <Stack.Screen
-        name="Ajouter une réservation"
-        component={AddBooking}
+       <Stack.Screen
+        name="Add"
+        component={
+          <RouteProtector permittedRoles={['ROLE_USER', 'ROLE_WAITER', 'ROLE_CEO']}>
+            {AddBooking}
+          </RouteProtector>
+        }
         options={headerTitle}
       />
 
       <Stack.Screen
-        name="Liste des réservations"
-        component={ListBookings}
+        name="Edit"
+        component={
+          <RouteProtector permittedRoles={['ROLE_USER', 'ROLE_WAITER', 'ROLE_CEO']}>
+            {AddBooking}
+          </RouteProtector>
+        }
         options={headerTitle}
-      /> */}
+      />
+
+      <Stack.Screen
+        name="Cancel"
+        component={
+          <RouteProtector permittedRoles={['ROLE_USER', 'ROLE_WAITER', 'ROLE_CEO']}>
+            {AddBooking}
+          </RouteProtector>
+        }
+        options={headerTitle}
+      />
+      
     </Stack.Navigator>
   );
 }
